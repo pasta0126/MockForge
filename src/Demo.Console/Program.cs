c3e4e70ae0c90124f7bb0d01f36ba7ee70dd4021
Guille Garcia <pasta0126@gmail.com>
2025-09-20 12:06:39 +0200

    demo console

---
+++ src/Demo.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using MockForge;
+using MockForge.Models;
+using MockForge.Providers;
+using System.Text.Json;
+
+var forge = MockForgeFactory.Create();
+
+var answerProvider = forge.Get<AnswerProvider>();
+var magicBall = answerProvider.Magic8Ball();
+Console.WriteLine($"Magic 8 Ball: {magicBall}");
+
+
+Console.WriteLine();
+
+var identityProvider = forge.Get<IdentityProvider>();
+Person person = identityProvider.Person(withNobelTitle: false, withTitle: false, withMiddelName: false);
+
+Console.WriteLine("Persona:");
+
+var jsonOptions = new JsonSerializerOptions
+{
+    WriteIndented = true,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+};
+string personJson = JsonSerializer.Serialize(person, jsonOptions);
+Console.WriteLine(personJson);
