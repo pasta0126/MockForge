dfe93e53794cee1a7e34bdc228f0c153b027b99f
Guille Garcia <pasta0126@gmail.com>
2025-11-26 16:22:22 +0100

    commit

--- src/Demo.Console/Program.cs
+++ src/Demo.Console/Program.cs
@@ -5,32 +5,12 @@ using System.Text.Json;
 
 var forge = MockForgeFactory.Create();
 
-var answerProvider = forge.Get<AnswerProvider>();
-var magicBall = answerProvider.Magic8Ball();
-Console.WriteLine($"Magic 8 Ball: {magicBall}");
-
-
-Console.WriteLine();
-
-var identityProvider = forge.Get<IdentityProvider>();
-Person person = identityProvider.Person(withNobelTitle: false, withTitle: false, withMiddelName: false);
-
-Console.WriteLine("Persona:**");
-
 var jsonOptions = new JsonSerializerOptions
 {
-    WriteIndented = true,
-    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    WriteIndented = true
 };
-string personJson = JsonSerializer.Serialize(person, jsonOptions);
-Console.WriteLine(personJson);
 
-var colorProvider = forge.Get<ColorProvider>();
-var hexColor = colorProvider.GetHexColor(person.FullName);
-Console.WriteLine($"Color HEX para '{person.FullName}': {hexColor}");
-
-Console.WriteLine();
-Console.WriteLine("Apartamento:**");
+Console.WriteLine("Apartamento:");
 
 var housingProvider = forge.Get<HousingProvider>();
 ApartmentDefinition apartment = housingProvider.Generate(1, 120);
